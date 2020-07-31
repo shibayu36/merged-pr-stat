eea115d04b06e83e6a6798ce741aee20557c79f0
shibayu36 <shibayu36@gmail.com>
2020-07-31 09:57:47 +0900

    Specify webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const path = require("path");
+
 module.exports = {
   mode: "production",
   entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
   target: "node",
   module: {
     rules: [
