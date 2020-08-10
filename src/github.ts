8fdaf96f7a753d199f12cf0eaa32a1ebf5465b09
shibayu36 <shibayu36@gmail.com>
2020-08-11 00:24:20 +0900

    Move fetchMergedPullRequests method to github.ts

--- src/github.ts
+++ src/github.ts
@@ -1,4 +1,6 @@
-import { GraphQLClient } from "graphql-request";
+import { GraphQLClient, gql } from "graphql-request";
+import { PullRequest } from "./types";
+import { parseISO } from "date-fns";
 
 const GITHUB_GRAPHQL_ENDPOINT = "https://api.github.com/graphql";
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
@@ -8,3 +10,68 @@ export const graphQLClient = new GraphQLClient(GITHUB_GRAPHQL_ENDPOINT, {
     authorization: `Bearer ${GITHUB_TOKEN}`,
   },
 });
+
+export async function fetchAllMergedPullRequests(
+  searchQuery: string,
+  startDateString?: string,
+  endDateString?: string
+): Promise<PullRequest[]> {
+  const startDate = startDateString ? parseISO(startDateString).toISOString() : "";
+  const endDate = endDateString ? parseISO(endDateString).toISOString() : "";
+
+  let q = `is:pr is:merged ${searchQuery}`;
+  if (startDate !== "" || endDate !== "") {
+    q += ` merged:${startDate}..${endDate}`;
+  }
+
+  return fetchAllPullRequestsByQuery(q);
+}
+
+async function fetchAllPullRequestsByQuery(searchQuery: string): Promise<PullRequest[]> {
+  const query = gql`
+    query($after: String) {
+      search(type: ISSUE, first: 100, query: "${searchQuery}", after: $after) {
+        issueCount
+        nodes {
+          ... on PullRequest {
+            title
+            author {
+              login
+            }
+            url
+            createdAt
+            mergedAt
+            additions
+            deletions
+          }
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+      rateLimit {
+        limit
+        cost
+        remaining
+        resetAt
+      }
+    }
+  `;
+
+  let after: string | undefined;
+  let prs: PullRequest[] = [];
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const data = await graphQLClient.request(query, { after });
+    prs = prs.concat(data.search.nodes as PullRequest[]);
+
+    if (!data.search.pageInfo.hasNextPage) break;
+
+    console.error(JSON.stringify(data, undefined, 2));
+
+    after = data.search.pageInfo.endCursor;
+  }
+
+  return prs;
+}
--- src/log-command.ts
+++ src/log-command.ts
@@ -1,7 +1,4 @@
-import { parseISO } from "date-fns";
-import { gql } from "graphql-request";
-import { graphQLClient } from "./github";
-import { PullRequest } from "./types";
+import { fetchAllMergedPullRequests } from "./github";
 
 interface LogCommandOptions {
   start: string;
@@ -9,60 +6,6 @@ interface LogCommandOptions {
   query: string;
 }
 export async function logCommand(options: LogCommandOptions): Promise<void> {
-  const startDate = parseISO(options.start);
-  const endDate = parseISO(options.end);
-  const query = options.query;
-
-  const searchQuery = `is:pr is:merged merged:${startDate.toISOString()}..${endDate.toISOString()} ${query}`;
-  const prs = await fetchAllPullRequestsByQuery(searchQuery);
+  const prs = await fetchAllMergedPullRequests(options.query, options.start, options.end);
   process.stdout.write(JSON.stringify(prs, undefined, 2));
-}
-
-async function fetchAllPullRequestsByQuery(searchQuery: string): Promise<PullRequest[]> {
-  const query = gql`
-    query($after: String) {
-      search(type: ISSUE, first: 100, query: "${searchQuery}", after: $after) {
-        issueCount
-        nodes {
-          ... on PullRequest {
-            title
-            author {
-              login
-            }
-            url
-            createdAt
-            mergedAt
-            additions
-            deletions
-          }
-        }
-        pageInfo {
-          endCursor
-          hasNextPage
-        }
-      }
-      rateLimit {
-        limit
-        cost
-        remaining
-        resetAt
-      }
-    }
-  `;
-
-  let after: string | undefined;
-  let prs: PullRequest[] = [];
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const data = await graphQLClient.request(query, { after });
-    prs = prs.concat(data.search.nodes as PullRequest[]);
-
-    if (!data.search.pageInfo.hasNextPage) break;
-
-    console.error(JSON.stringify(data, undefined, 2));
-
-    after = data.search.pageInfo.endCursor;
-  }
-
-  return prs;
 }
