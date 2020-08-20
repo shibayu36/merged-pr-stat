4460c502d4062e59d52697238dcf41c1cc083cff
shibayu36 <shibayu36@gmail.com>
2020-08-20 22:18:44 +0900

    stat is default

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,21 @@ import { statCommand } from "./stat-command";
 
 async function main(): Promise<void> {
   program.version("0.1.0");
+
+  program
+    .command("stat", { isDefault: true })
+    .option("--input <filepath>")
+    .option("--start <date>")
+    .option("--end <date>")
+    .option("--query <search query>")
+    .action(statCommand);
+
   program
     .command("log")
     .requiredOption("--start <date>", "start date")
     .requiredOption("--end <date>", "end date")
     .requiredOption("--query <search query>", "query for github search")
     .action(logCommand);
-
-  program
-    .command("stat")
-    .option("--input <filepath>")
-    .option("--start <date>")
-    .option("--end <date>")
-    .option("--query <search query>")
-    .action(statCommand);
 
   program.parse(process.argv);
 }
