86b4a3e126c6ad43d30b24f4d288a5a31956668e
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2023-10-10 11:48:10 +0200

    Refactor: extract `sum` function for consistency with existing code
    Other aggregations such as `additionsAverage` aggregate the average of the provided addition numbers with the `average` function, so it feels more consisten if we have a `sum` function for sum aggregations as well.

--- src/stat-command.ts
+++ src/stat-command.ts
@@ -51,10 +51,10 @@ export function createStat(prs: PullRequest[]): PullRequestStat {
   return {
     count: prs.length,
     authorCount: uniq(prs.map((pr) => pr.author)).length,
-    additions: prs.map((pr) => pr.additions).reduce((a, b) => a + b, 0),
+    additions: sum(prs.map((pr) => pr.additions)),
     additionsAverage: average(prs.map((pr) => pr.additions)),
     additionsMedian: median(prs.map((pr) => pr.additions)),
-    deletions: prs.map((pr) => pr.deletions).reduce((a, b) => a + b, 0),
+    deletions: sum(prs.map((pr) => pr.deletions)),
     deletionsAverage: average(prs.map((pr) => pr.deletions)),
     deletionsMedian: median(prs.map((pr) => pr.deletions)),
     leadTimeSecondsAverage: Math.floor(average(leadTimes)),
@@ -74,6 +74,10 @@ function average(numbers: number[]): number {
 function median(numbers: number[]): number {
   if (numbers.length === 0) return 0;
   return _median(numbers);
+}
+
+function sum(numbers: number[]): number {
+  return numbers.reduce((a, b) => a + b, 0);
 }
 
 export function createPullRequestsByLog(path: string): PullRequest[] {
